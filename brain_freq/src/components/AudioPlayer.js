0feeb6bee7970059e93f98dff7939a458c9bbb07
Chris Gordon <chris.c.gordon888@gmail.com>
2023-07-13 18:59:07 -0600

    Updated starter files to include workin media player. Updated Tracklist for our project.

---
+++ brain_freq/src/components/AudioPlayer.js
@@ -0,0 +1,77 @@
+import { useRef, useState, useEffect } from 'react';
+import { tracks } from '../data/tracks';
+import DisplayTrack from './DisplayTrack';
+import Controls from './Controls';
+import ProgressBar from './ProgressBar';
+import '../styles/brain-freq-styles.css';
+import '../styles/cosmic-animations.css';
+
+const AudioPlayer = () => {
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [currentTrack, setCurrentTrack] = useState(tracks[trackIndex]);
+  const [timeProgress, setTimeProgress] = useState(0);
+  const [duration, setDuration] = useState(0);
+
+  // reference
+  const audioRef = useRef();
+  const progressBarRef = useRef();
+
+  // Log the current track
+  useEffect(() => {
+    console.log(`currentTrack: ${JSON.stringify(currentTrack)}`);
+  }, [currentTrack]);
+
+  const handleNext = () => {
+    if (trackIndex >= tracks.length - 1) {
+      setTrackIndex(0);
+      setCurrentTrack(tracks[0]);
+    } else {
+      setTrackIndex((prev) => prev + 1);
+      setCurrentTrack(tracks[trackIndex + 1]);
+    }
+  };
+  
+  return (
+    <div className="app cosmic-theme cosmic-body">
+      <header className="app-header">
+          <h1 className="app-title">Brain Freq</h1>
+          <nav className="app-nav">
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Contact</a>
+          </nav>
+      </header>
+      <div className="audio-player">
+        <div className="inner">
+          <DisplayTrack {...{ currentTrack, audioRef, setDuration, progressBarRef, handleNext }} />
+          <Controls {...{ 
+            audioRef, 
+            progressBarRef, 
+            duration, 
+            setTimeProgress, 
+            tracks, 
+            trackIndex, 
+            setTrackIndex, 
+            setCurrentTrack, 
+            handleNext
+          }} />
+          <ProgressBar {...{ progressBarRef, audioRef, timeProgress, duration }} />
+        </div>
+      </div>
+      <footer className="app-footer">
+    <pre className="ascii-art">
+      _    _ _______ _____ ____  _   _ 
+     | |  | |__   __|_   _/ __ \| \ | |
+     | |  | |  | |    | || |  | |  \| |
+     | |  | |  | |    | || |  | | . ` |
+     | |__| |  | |   _| || |__| | |\  |
+      \____/   |_|  |_____\____/|_| \_|
+    </pre>
+    <p>&copy; 2023 Brain Freq. All rights reserved.</p>
+</footer>
+    </div>
+  );
+
+};
+
+export default AudioPlayer;
