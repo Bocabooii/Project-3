67baf50260b81f47de8cdb22a176e9726057147b
fibikunle <43515489+fibikunle@users.noreply.github.com>
2023-07-24 18:52:09 -0500

    login/signup

--- brain_freq/client/src/App.js
+++ brain_freq/client/src/App.js
@@ -18,7 +18,7 @@ import Login from './pages/Login';
 
 const httpLink = createHttpLink({
   // uri: '/graphql',
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://localhost:3001/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
--- brain_freq/server/server.js
+++ brain_freq/server/server.js
@@ -7,7 +7,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 const server = new ApolloServer({
   typeDefs,
@@ -18,6 +18,9 @@ const server = new ApolloServer({
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Serve up static assets
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -25,6 +28,7 @@ if (process.env.NODE_ENV === 'production') {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
