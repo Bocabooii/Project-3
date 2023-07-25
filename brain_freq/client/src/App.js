33eda0bf3e82ff3983443d8140ade4b00cadcc19
fibikunle <43515489+fibikunle@users.noreply.github.com>
2023-07-25 12:58:20 -0500

    signup & login page

--- brain_freq/client/src/App.js
+++ brain_freq/client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import { Login } from './components';
+import Signup from './pages/Signup';
+// import { Login } from './components';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
 
       </Routes>
